d5a3eafc0b41e77331d87df19a1395418ebdd955
윤정훈 <yunjunghun0116@gmail.com>
2021-08-25 13:36:14 +0900

    hash table && tree

--- pythonPractice/hash.py
+++ pythonPractice/hash.py
@@ -8,6 +8,7 @@ hash_table = list([0 for i in range(10)])
 def make_hash(key):
     return key%10
 
+#단순 해쉬테이블
 #ord : ASCII코드 반환
 def storage_data(data,value):
     key = ord(data[0])
@@ -17,40 +18,44 @@ def get_data(data):
     key = ord(data[0])
     hash_address = make_hash(key)
     return hash_table[hash_address]
-    
-storage_data('Andy', '01055553333')
-storage_data('Dave', '01044443333')
-storage_data('Trump', '01022223333')
 
-print(get_data('Dave'))
+####SHA-1/256 ######
+#import hashlib -> hashlib라는 라이브러리에서 가져오고
+#data = 'input'.encode() -> byte형태로 변환시켜준후에
+#hash_object = hashlib.sha1() -> sha1/sha256 을 이용하면된다.
+#hash_object.update(data) 또한 data가 아닌 b'input'으로 하면 input이라는 string의 byte형태가 입력되게된다.
+#print(hash_object.hexdigest()) -> 문자열의 형태로 해쉬화된 결과값이나오게된다
+#만약 문자열이 16진수로 나타나게하고싶을때는
+#int(hash_object.hexdigest(),16)을 이용해서 문자->상수 로 형변환이가능하다.
 
 #해쉬충돌해결알고리즘 - 해싱한 결과가 같은경우가 존재할수있고,
 #그경우 충돌이 발생할수있기에 해쉬테이블을 이용할때에는
 #충돌해결알고리즘또한 고안해야한다.
 
-#1.개방해싱 - 충돌시 링크드리스트를 통해 추가로 뒤에 연결시키기
 def get_key(data):
-    return hash(data)
+    hash_key = hash(data)
+    if hash_key < 0:
+        return -hash_key
+    return hash_key
 
-#실제로는 돌아가지않으나 Node처럼 next의 형태를 구현하기만 하면 될듯
 def save_data(data,value):
     index_key = get_key(data)
     hash_address = make_hash(index_key)
-    if(hash_table[hash_address] != 0):
+    if hash_table[hash_address] != 0:
         for i in range(len(hash_table[hash_address])):
-            if hash_table[hash_address][i][0] == index_key:
+            if hash_table[hash_address][i][0] == data:
                 hash_table[hash_address][i][1] = value
                 return
-        hash_table[hash_address].append([index_key, value])
+        hash_table[hash_address].append([data, value])
     else:
-        hash_table[hash_address] = [[index_key,value]]
+        hash_table[hash_address] = [[data,value]]
 
 def read_data(data):
     index_key = get_key(data)
     hash_address = make_hash(index_key)
     if hash_table[hash_address] != 0:
         for index in range(len(hash_table[hash_address])):
-            if hash_table[hash_address][index][0] == index_key:
+            if hash_table[hash_address][index][0] == data:
                 return hash_table[hash_address][index][1]
         return None
     else:
@@ -59,8 +64,11 @@ def read_data(data):
 
 save_data('Junghun','01012341234')
 save_data('Dd', '1201023010')
+save_data('Dh', '1201024243010')
+save_data('Dc', '12010242430910')
 save_data('Data', '3301023010')
-read_data('Junghun')
+print(hash_table)
+print(read_data('Dd'))
 
 
 
---
+++ pythonPractice/tree.py
@@ -0,0 +1,8 @@
+###용어###
+# Node : 트리에서 데이터를 저장하는 기본요소,데이터와 다른 연결된 노드에 대한 Branch 정보(Left/Right)
+# Root Node: 트리 최상단에 위치하는 노드
+# Level : 최상위노드를 level0으로 할때 하위 branch로 연결된 노드의 깊이
+# Parent Node - Child Node : 상단/하단에 위치한 두 노드
+# Leaf Node(Terminal Node) : Child Node가 하나도 없는 노드
+# Sibling : 동일한 Parent Node를 가진 노드
+# Depth : 트리의 노드가 가질수 있는 최대 Level
