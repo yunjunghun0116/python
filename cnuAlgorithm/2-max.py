dfd0eaea7d7e718464913af3bda42fa8d9c46a65
윤정훈 <yunjunghun0116@gmail.com>
2021-09-06 17:54:15 +0900

    complexity check with python library matplotlib

--- cnuAlgorithm/2-max.py
+++ cnuAlgorithm/2-max.py
@@ -1,13 +1,50 @@
+import random
+import time
+import matplotlib.pyplot as plt
+
 def main():
-    max = 0
-    while True:
-        try:
-            n = int(input('Input Num : '))
-            if n > max:
-                max = n
-        except EOFError:
-            break
-        print('Max : ',max)
+    SortimeTimes = []
+    FindTimes = []
+    for listSize in range(100000,1000000,200000):
+        alist = [random.randrange(1000) for x in range(listSize)]
+        start = time.time()
+        find_max(alist)
+        end = time.time()
+        runTime = end - start
+        FindTimes.append(runTime)
+
+        start = time.time()
+        find_max_sorting(alist)
+        end = time.time()
+        runTime = end - start
+        SortimeTimes.append(runTime)
+
+    sizes = range(100000,1000000,200000)
+    plt.figure(figsize=(8,5))
+    plt.plot(sizes, SortimeTimes,marker='x',c='r',label='findMaxTimeSorting'),
+    plt.plot(sizes, FindTimes,marker='x',c='g',label='findMaxTime'),
+    plt.xlabel('Input size')
+    plt.ylabel('Time (second)')
+    plt.legend(loc = 2)
+    plt.grid()
+    plt.title('Complexity')
+    plt.savefig('complexity.png',bbox_inches='tight')
+
+
+
+
+
+def find_max(input_list):
+    temp_max = input_list[0]
+    for i in input_list:
+        if i > temp_max:
+            temp_max = i
+    return temp_max
+
+def find_max_sorting(input_list):
+    input_list.sort()
+    return input_list[-1]
+    
 
 if __name__ == '__main__':
     main()
