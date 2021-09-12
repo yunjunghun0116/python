4e2d955b6e5c3ac8950e0b1fc03d3f4de8091601
윤정훈 <yunjunghun0116@gmail.com>
2021-09-12 17:10:19 +0900

    bfs

--- Algorithm/그래프/bfs.py
+++ Algorithm/그래프/bfs.py
@@ -1,2 +1,36 @@
 # BFS 알고리즘 구현
 # 자료구조 Queue를 사용한다
+import queue
+graph = dict()
+
+graph['a'] = ['b','c']
+graph['b'] = ['a','d']
+graph['c'] = ['a','g','h','i']
+graph['d'] = ['b','e','f']
+graph['e'] = ['d']
+graph['f'] = ['d']
+graph['g'] = ['c']
+graph['h'] = ['c']
+graph['i'] = ['c','j']
+graph['j'] = ['i']
+
+
+
+def bfs(graph,start_val):
+    bfs_queue = queue.Queue()
+    visited = []
+    bfs_queue.put(start_val)
+    while bfs_queue.qsize() != 0:
+        data = bfs_queue.get()
+        print(data)
+        for i in range(len(graph[data])):
+            if visited.__contains__(graph[data][i]):
+                continue
+            else:
+                bfs_queue.put(graph[data][i])
+                visited.append(graph[data][i])
+        visited.append(start_val)
+
+
+bfs(graph,'a')
+
