d88290879c40298271d4bfda17d51349c7f80348
윤정훈 <yunjunghun0116@gmail.com>
2021-10-09 00:49:57 +0900

    드디어 맞았다!!.

--- baekjoon/뒤집기게임.py
+++ baekjoon/뒤집기게임.py
@@ -2,8 +2,14 @@ import sys
 
 row_result = []
 row_cnt_result = []
+# 행렬 전환후에
+changed_result = []
+
 col_result = []
 col_cnt_result = []
+
+final_result = []
+
 def dfs(board,size,current,cnt):
     if len(current) == size:
         row_result.append(current[:])
@@ -19,45 +25,60 @@ def dfs(board,size,current,cnt):
     current_board.append(row_list_swap)
     dfs(board,size,current_board,cnt+1)
 
+def row_col_change(board,size):
+    temp_board = [[board[i][j] for j in range(size)]for i in range(size)]
+    for i in range(size):
+        for j in range(i+1):
+            temp = temp_board[i][j]
+            temp_board[i][j] = temp_board[j][i]
+            temp_board[j][i] = temp
+    return temp_board
+
 def col_change(board,size,current,cnt):
-    if len(current[0]) == size:
+    if len(current) == size:
         col_result.append(current[:])
         col_cnt_result.append(cnt)
         return
-    curr_col = len(current[0])
+    curr_row = len(current)
     current_board = current[:]
-    for i in range(size):
-        current_board[i].append(board[i][curr_col])
-    col_change(board,size,current_board,cnt+1)
-    for i in range(size):
-        current_board[i][curr_col] = 1 - current_board[i][curr_col]
+    row_append = board[curr_row][:]
+    current_board.append(row_append)
+    col_change(board,size,current_board,cnt)
+    row_list_swap = [1 - row_append[i] for i in range(size)]
+    current_board.pop()
+    current_board.append(row_list_swap)
     col_change(board,size,current_board,cnt+1)
 
-
+def check(board,size,cnt):
+    cnt_0 = 0
+    cnt_1 = 0
+    for i in range(size):
+        for j in range(size):
+            if board[i][j] == 0:
+                cnt_0 += 1
+            if board[i][j] == 1:
+                cnt_1 += 1
+    final_cnt = cnt + min(cnt_0,cnt_1)
+    final_result.append(final_cnt)
+    
 def main():
     size = int(sys.stdin.readline())
     board = [list(map(int,sys.stdin.readline().strip().split())) for _ in range(size)]
-    
     dfs(board,size,[],0)
-
     for i in range(len(row_result)):
-        print(i,'번째')
-        for j in range(size):
-            print(row_result[i][j])
-        
-
+        temp_result = row_col_change(row_result[i],size)
+        changed_result.append(temp_result)
+    # 여기까지 행렬변환 성공
     for i in range(len(row_result)):
-        start_col = [[] for _ in range(size)]
-        col_change(row_result[i],size,start_col,0)
-    
+        curr_board = changed_result[i]
+        curr_cnt = row_cnt_result[i]
+        col_change(curr_board,size,[],curr_cnt)
+    # col_result에는 모든 경우의 수가 저장
+    # col_cnt_result = 모든 cnt가 저장
     for i in range(len(col_result)):
-        for j in range(size):
-            print(col_result[i][j])
+        check(col_result[i],size,col_cnt_result[i])
 
-
-    
-        
-
+    print(min(final_result))
 
 if __name__ == '__main__':
     main()
