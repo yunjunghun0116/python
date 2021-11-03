160ae4870c13f77b091520c46215fea3ed649e0f
윤정훈 <yunjunghun0116@gmail.com>
2021-11-04 01:39:45 +0900

    실습7주차 1,2 풀이

--- cnuAlgorithm/실습7주차/1.py
+++ cnuAlgorithm/실습7주차/1.py
@@ -5,12 +5,15 @@ def solve(trains,train_line):
     stack = []
     visited.append(trains[0])
     stack.extend(train_line[trains[0]])
+    # trains[0]에서부터 갈수있는 모든곳을 연결해본후 만약 모든 곳을
+    # 방문한다면 visited의 원소 개수가
+    # 모든 기차역의 개수와 동일하기때문에
     while stack:
         pop_data = stack.pop()
         if pop_data not in visited:
             visited.append(pop_data)
             stack.extend(train_line[pop_data])
-
+    # 같은경우 True를 리턴해주고 아니면 False를 리턴해준다.
     if len(trains) == len(visited):
         return True
     else:
--- cnuAlgorithm/실습7주차/2.py
+++ cnuAlgorithm/실습7주차/2.py
@@ -2,10 +2,14 @@ import sys
 
 def solve(base,stations):
     result = 0
+    # 모든 기지에서부터 각 기지와 전력소간 최단거리를 구한후
     for b in base:
         distance_min = float('inf')
         for s in stations:
             distance_min = min(distance_min,abs(b-s))
+        # 여기서 distance_min이 최단거리이기때문에
+        # 그 최단거리들사이 최댓값을 출력해주면 그 값이 결국 모든 기지가
+        # 전력공급망에 포함하는 최소한의 추가전력생산량이다.
         result = max(result,distance_min)
     return result
     
